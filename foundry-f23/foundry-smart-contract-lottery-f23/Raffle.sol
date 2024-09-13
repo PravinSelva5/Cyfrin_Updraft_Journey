92898c2cfea9d72d920bfe5a2732ce2151d8cd72
Pravin Selvarajah <pravin.selvarajah.eng@gmail.com>
2024-09-12 22:28:46 -0400

    foundry smart contract lottery project

---
+++ foundry-f23/foundry-smart-contract-lottery-f23/Raffle.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/// @title A sample Raffle contract
+/// @author Pravin Selvarajah
+/// @notice This contract is for creating a sample raffle 
+/// @dev Implements Chainlink VRFv2.5
+
+contract Raffle {
+    function enterRaffle() public {
+        
+    }
+
+    function pickWinner() public {}
+
+}
