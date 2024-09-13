e648d631027cc13290cdc0a9ffbf5a68571fa059
Pravin Selvarajah <pravin.selvarajah.eng@gmail.com>
2024-09-12 22:43:58 -0400

    chore: forge init

--- foundry-f23/foundry-smart-contract-lottery-f23/Raffle.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/Raffle.sol
@@ -7,10 +7,22 @@ pragma solidity ^0.8.19;
 /// @dev Implements Chainlink VRFv2.5
 
 contract Raffle {
-    function enterRaffle() public {
-        
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    } 
+    
+    function enterRaffle() public payable {
+
     }
 
     function pickWinner() public {}
+
+
+    /** Getter Functions **/
+    function getEntranceFee() external view returns(uint256) {
+        return i_entranceFee;
+    }
 
 }
